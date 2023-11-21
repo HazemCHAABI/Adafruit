96830f8bbe567bfb6105b6a8e3b24b728e73aa75
HazemCHAABI <65767801+HazemCHAABI@users.noreply.github.com>
2023-11-21 15:41:51 +0100

    File added

---
+++ AdafruitIO.py
@@ -0,0 +1,116 @@
+from network import WLAN      # For operation of WiFi network
+import time                   # Allows use of time.sleep() for delays
+import pycom                  # Base library for Pycom devices
+from umqtt import MQTTClient  # For use of MQTT protocol to talk to Adafruit IO
+import ubinascii              # Needed to run any MicroPython code
+import machine                # Interfaces with hardware components
+import micropython            # Needed to run any MicroPython code
+
+# BEGIN SETTINGS
+# These need to be change to suit your environment
+RANDOMS_INTERVAL = 5000 # milliseconds
+last_random_sent_ticks = 0  # milliseconds
+
+# Wireless network
+WIFI_SSID = "Galaxy Note10+2587"
+WIFI_PASS = "qilw3366" # No this is not our regular password. :)
+
+# Adafruit IO (AIO) configuration
+AIO_SERVER = "io.adafruit.com"
+AIO_PORT = 1883
+AIO_USER = "HZM_911"
+AIO_KEY = "aio_XGcS98zHGmxvsF5duJ6W2S1dMelb"
+AIO_CLIENT_ID = ubinascii.hexlify(machine.unique_id())  # Can be anything
+AIO_CONTROL_FEED = "HZM_911/feeds/lights"
+AIO_RANDOMS_FEED = "HZM_911/feeds/randoms"
+
+# END SETTINGS
+
+# RGBLED
+# Disable the on-board heartbeat (blue flash every 4 seconds)
+# We'll use the LED to respond to messages from Adafruit IO
+pycom.heartbeat(False)
+time.sleep(0.1) # Workaround for a bug.
+                # Above line is not actioned if another
+                # process occurs immediately afterwards
+pycom.rgbled(0xff0000)  # Status red = not working
+
+# WIFI
+# We need to have a connection to WiFi for Internet access
+# Code source: https://docs.pycom.io/chapter/tutorials/all/wlan.html
+
+wlan = WLAN(mode=WLAN.STA)
+wlan.connect(WIFI_SSID, auth=(WLAN.WPA2, WIFI_PASS), timeout=5000)
+
+while not wlan.isconnected():    # Code waits here until WiFi connects
+    machine.idle()
+
+print("Connected to Wifi")
+pycom.rgbled(0xffd7000) # Status orange: partially working
+
+# FUNCTIONS
+
+# Function to respond to messages from Adafruit IO
+def sub_cb(topic, msg):          # sub_cb means "callback subroutine"
+    print((topic, msg))          # Outputs the message that was received. Debugging use.
+    if msg == b"1":             # If message says "ON" ...
+        pycom.rgbled(0xff0000)   
+    elif msg == b"2":         
+        pycom.rgbled(0x00ff00)
+    elif msg == b"3":          
+        pycom.rgbled(0x0000ff)
+    elif msg == b"4":          
+        pycom.rgbled(0xff00ff)
+    elif msg == b"5":        
+        pycom.rgbled(0xffff00)
+    elif msg == b"6":        
+        pycom.rgbled(0x00ffff)
+    elif msg == b"0":          
+        pycom.rgbled(0x000000)   # ... then LED off
+    else:                        # If any other message is received ...
+        print("Unknown message") # ... do nothing but output that it happened.
+
+def random_integer(upper_bound):
+    return machine.rng() % upper_bound
+
+def send_random():
+    global last_random_sent_ticks
+    global RANDOMS_INTERVAL
+
+    if ((time.ticks_ms() - last_random_sent_ticks) < RANDOMS_INTERVAL):
+        return; # Too soon since last one sent.
+
+    some_number = random_integer(100)
+    print("Publishing: {0} to {1} ... ".format(some_number, AIO_RANDOMS_FEED), end='')
+    try:
+        client.publish(topic=AIO_RANDOMS_FEED, msg=str(some_number))
+        print("DONE")
+    except Exception as e:
+        print("FAILED")
+    finally:
+        last_random_sent_ticks = time.ticks_ms()
+
+# Use the MQTT protocol to connect to Adafruit IO
+client = MQTTClient(AIO_CLIENT_ID, AIO_SERVER, AIO_PORT, AIO_USER, AIO_KEY)
+
+# Subscribed messages will be delivered to this callback
+client.set_callback(sub_cb)
+client.connect()
+client.subscribe(AIO_CONTROL_FEED)
+print("Connected to %s, subscribed to %s topic" % (AIO_SERVER, AIO_CONTROL_FEED))
+
+pycom.rgbled(0x00ff00) # Status green: online to Adafruit IO
+
+try:                      # Code between try: and finally: may cause an error
+                          # so ensure the client disconnects the server if
+                          # that happens.
+    while 1:              # Repeat this loop forever
+        client.check_msg()# Action a message if one is received. Non-blocking.
+        send_random()     # Send a random number to Adafruit IO if it's time.
+finally:                  # If an exception is thrown ...
+    client.disconnect()   # ... disconnect the client and clean up.
+    client = None
+    wlan.disconnect()
+    wlan = None
+    pycom.rgbled(0x000022)# Status blue: stopped
+    print("Disconnected from Adafruit IO.")
